df1ae4efb44071556579c4d3e6f84edf92a192f4
Kimio <34326957+kojr1234@users.noreply.github.com>
2018-03-04 16:08:57 -0300

    Corrected all

--- objects/GameResources.py
+++ objects/GameResources.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os, pygame
 
 def load_image(name):
@@ -13,19 +12,3 @@ def load_image(name):
     except pygame.error:
         print("Oops! Could not load image:", fullname)
     return image, image.get_rect()
-=======
-import os, pygame
-
-def load_image(name):
-    """A better load of images."""
-    fullname = os.path.join("images", name)
-    try:
-        image = pygame.image.load(fullname)
-        if image.get_alpha() == None:
-            image = image.convert()
-        else:
-            image = image.convert_alpha()
-    except pygame.error:
-        print("Oops! Could not load image:", fullname)
-    return image, image.get_rect()
->>>>>>> b102c115033200c8a0aa8489b5e8bb21aa3c03f2
